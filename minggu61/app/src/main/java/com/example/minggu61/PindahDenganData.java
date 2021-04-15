d7ed7f7fefdd6b3ea233b4009611eb494af2cfd9
geofannyher <geofannyher@gmail.com>
2021-04-15 18:10:22 +0700

    androidming6.1

---
+++ minggu61/app/src/main/java/com/example/minggu61/PindahDenganData.java
@@ -0,0 +1,14 @@
+package com.example.minggu61;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PindahDenganData extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pindah_dengan_data);
+    }
+}
